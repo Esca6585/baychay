463c63e65896d22b4170d4061af2fcddd98bdc4d
Esca6585 <esca656585@gmail.com>
2021-09-14 10:43:09 +0500

    updated_banner_slider

--- config/languages.php
+++ config/languages.php
@@ -2,16 +2,19 @@
     return [
         'tm' => [
             'name' => 'Türkmen',
-            'icon' => '229-turkmenistan.svg'
+            'icon' => '229-turkmenistan.svg',
+            'slider' => 'baychay-template/img/slider/baychay-banner-tm.jpg'
         ],
         
         'en' => [
             'name' => 'English',
-            'icon' => '226-united-states.svg'
+            'icon' => '226-united-states.svg',
+            'slider' => 'baychay-template/img/slider/baychay-banner-en.jpg'
         ],
 
         'ru' => [
             'name' => 'Русский',
-            'icon' => '248-russia.svg'
+            'icon' => '248-russia.svg',
+            'slider' => 'baychay-template/img/slider/baychay-banner-ru.jpg'
         ],
     ];
