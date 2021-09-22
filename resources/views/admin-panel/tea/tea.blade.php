226bbc8e16f5dfaa1d6cdbbb552e983b2317fd2e
Esca6585 <esca656585@gmail.com>
2021-09-22 15:45:10 +0500

    updated

--- resources/views/admin-panel/tea/tea.blade.php
+++ resources/views/admin-panel/tea/tea.blade.php
@@ -7,6 +7,7 @@
 @section('body')
 
 <!--begin::Body-->
+
 <body id="kt_body"
     class="header-fixed header-mobile-fixed subheader-enabled subheader-fixed aside-enabled aside-fixed aside-minimize-hoverable page-loading">
     <!--begin::Main-->
@@ -147,18 +148,27 @@
                                     <div class="card-toolbar">
                                         <!--begin::Button-->
                                         <a href="{{ route(Request::segment(3) . '.create', app()->getlocale() ) }}"
-                                            class="btn font-weight-bolder">
-                                            <span class="svg-icon svg-icon-xl">
-                                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                                    xmlns="http://www.w3.org/2000/svg">
-                                                    <path opacity="0.25" fill-rule="evenodd" clip-rule="evenodd"
-                                                        d="M6.54184 2.36899C4.34504 2.65912 2.65912 4.34504 2.36899 6.54184C2.16953 8.05208 2 9.94127 2 12C2 14.0587 2.16953 15.9479 2.36899 17.4582C2.65912 19.655 4.34504 21.3409 6.54184 21.631C8.05208 21.8305 9.94127 22 12 22C14.0587 22 15.9479 21.8305 17.4582 21.631C19.655 21.3409 21.3409 19.655 21.631 17.4582C21.8305 15.9479 22 14.0587 22 12C22 9.94127 21.8305 8.05208 21.631 6.54184C21.3409 4.34504 19.655 2.65912 17.4582 2.36899C15.9479 2.16953 14.0587 2 12 2C9.94127 2 8.05208 2.16953 6.54184 2.36899Z"
-                                                        fill="#12131A" />
-                                                    <path fill-rule="evenodd" clip-rule="evenodd"
-                                                        d="M12 17C12.5523 17 13 16.5523 13 16V13H16C16.5523 13 17 12.5523 17 12C17 11.4477 16.5523 11 16 11H13V8C13 7.44772 12.5523 7 12 7C11.4477 7 11 7.44772 11 8V11H8C7.44772 11 7 11.4477 7 12C7 12.5523 7.44771 13 8 13H11V16C11 16.5523 11.4477 17 12 17Z"
-                                                        fill="#12131A" />
+                                            class="btn btn-primary font-weight-bolder">
+                                            <span class="svg-icon svg-icon-md">
+                                                <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    xmlns:xlink="http://www.w3.org/1999/xlink">
+                                                    <!-- Generator: Sketch 50.2 (55047) - http://www.bohemiancoding.com/sketch -->
+                                                    <title>Stockholm-icons / Code / Plus</title>
+                                                    <desc>Created with Sketch.</desc>
+                                                    <defs></defs>
+                                                    <g id="Stockholm-icons-/-Code-/-Plus" stroke="none" stroke-width="1"
+                                                        fill="none" fill-rule="evenodd">
+                                                        <rect id="bound" x="0" y="0" width="24" height="24"></rect>
+                                                        <circle id="Oval-5" fill="#000000" opacity="0.3" cx="12" cy="12"
+                                                            r="10"></circle>
+                                                        <path
+                                                            d="M11,11 L11,7 C11,6.44771525 11.4477153,6 12,6 C12.5522847,6 13,6.44771525 13,7 L13,11 L17,11 C17.5522847,11 18,11.4477153 18,12 C18,12.5522847 17.5522847,13 17,13 L13,13 L13,17 C13,17.5522847 12.5522847,18 12,18 C11.4477153,18 11,17.5522847 11,17 L11,13 L7,13 C6.44771525,13 6,12.5522847 6,12 C6,11.4477153 6.44771525,11 7,11 L11,11 Z"
+                                                            id="Combined-Shape" fill="#000000"></path>
+                                                    </g>
                                                 </svg>
                                             </span>
+                                            {{ __('Create') }}
                                         </a>
                                         <!--end::Button-->
                                     </div>
@@ -253,7 +263,8 @@
 
                                     <!--begin::Alert-->
                                     @if(session()->has('success-create'))
-                                    <div class="alert alert-fixed alert-custom alert-light-primary fade show mb-5" id="alert-message" role="alert">
+                                    <div class="alert alert-fixed alert-custom alert-light-primary fade show mb-5"
+                                        id="alert-message" role="alert">
                                         <div class="alert-icon">
                                             <i class="flaticon2-plus text-primary"></i>
                                         </div>
@@ -269,7 +280,8 @@
                                     @endif
 
                                     @if(session()->has('success-update'))
-                                    <div class="alert alert-fixed alert-custom alert-light-warning fade show mb-5" id="alert-message" role="alert">
+                                    <div class="alert alert-fixed alert-custom alert-light-warning fade show mb-5"
+                                        id="alert-message" role="alert">
                                         <div class="alert-icon">
                                             <i class="flaticon2-edit text-warning"></i>
                                         </div>
@@ -283,9 +295,10 @@
                                         </div>
                                     </div>
                                     @endif
-                                    
+
                                     @if(session()->has('success-delete'))
-                                    <div class="alert alert-fixed alert-custom alert-light-danger fade show mb-5" id="alert-message" role="alert">
+                                    <div class="alert alert-fixed alert-custom alert-light-danger fade show mb-5"
+                                        id="alert-message" role="alert">
                                         <div class="alert-icon">
                                             <i class="flaticon-delete text-danger"></i>
                                         </div>
@@ -300,9 +313,9 @@
                                     </div>
                                     @endif
                                     <!--end::Alert-->
-                                    
+
                                     <!--begin: Datatable-->
-                                        @include('admin-panel.tea.tea-table')
+                                    @include('admin-panel.tea.tea-table')
                                     <!--end: Datatable-->
                                 </div>
                             </div>
